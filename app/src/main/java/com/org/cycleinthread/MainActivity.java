3daa99da3c1d2cdb4ca09614d80fced7fc658ec7
Ilya Petrash <ilya.petrash@inbox.ru>
2017-05-25 20:08:45 +0500

    Append two thread

--- app/src/main/java/com/org/cycleinthread/MainActivity.java
+++ app/src/main/java/com/org/cycleinthread/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
 
-        new AsyncRequest().execute();
+        // NOTE: Обычный запуск через execute, при этом таски заносятся в очередь и
+        // пока одна не завершится, другая ожидает
+//        new AsyncRequest().execute();
+
+        // NOTE: в отличии от execute, потоки запускаются сразу все
+        new AsyncRequest().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        new AsyncRequest2().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     class AsyncRequest extends AsyncTask<Void, Void, Void> {
@@ -38,13 +46,37 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
         @Override
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
 
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
             textView.setText(timeStamp);
+        }
+    }
+
+    class AsyncRequest2 extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (true) {
+                // call onProgressUpdate
+                publishProgress();
+
+                try {
+                    Thread.sleep(3000);
+
+                } catch (Exception e) {
+
+                }
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            textView2.setText(timeStamp);
         }
     }
 }
